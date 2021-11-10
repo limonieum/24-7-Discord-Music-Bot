a1d3ddcb719f5877046770256b462637b1192843
Saputra Pratama <80446109+mylaurelle@users.noreply.github.com>
2021-11-10 23:06:57 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# 24-7-Discord-Music-Bot
-
-how to use 24/7 discord music bot :
-- install npm
-- run="node index.js"
+`npm install`
---
+++ index.js
@@ -0,0 +1,41 @@
+// hosting free replit
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('RCC is alive!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// import config files
+const { TOKEN, CHANNEL, LIVE } = require("./config.json");
+const ytdl = require("ytdl-core");
+
+// import events loader and node event handler functions
+const { loadEvents } = require("./utility/loadEvents");
+const { loadNode } = require("./utility/loadNode");
+
+// Discord client
+const { Client } = require("discord.js");
+
+const client = new Client();
+
+// error if no token provided, and error if channel id and yt url aren't valid
+if (!TOKEN) {
+  console.error("Please provide a valid Discord Bot Token.");
+  process.exit(1);
+} else if (!CHANNEL || Number(CHANNEL) == NaN) {
+  console.log("Please provide a valid channel ID.");
+  process.exit(1);
+} else if (!ytdl.validateURL(LIVE)) {
+  console.log("Please provide a valid Youtube URL.");
+  process.exit(1);
+}
+
+// login
+client.login(TOKEN).then(() => {
+  console.log(` Successfully logged in as: ${client.user.username}#${client.user.discriminator}`);
+})
+
+// run events loader and node events handler functions
+loadEvents(client, process);
+loadNode(process);
